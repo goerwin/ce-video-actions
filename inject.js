7218862bad0e893e405795a7ee1700827e7b531e
Erwin G <erwingaitano@gmail.com>
2026-07-27 01:08:28 -0500

    fix: youtube video actions improved

--- inject.js
+++ inject.js
@@ -1,97 +1,47 @@
 (() => {
-  if (window._ceVideoActionsDidRun) return;
-  window._ceVideoActionsDidRun = true;
+	if (window._ceVideoActionsDidRun) return;
+	window._ceVideoActionsDidRun = true;
 
-  const { hostname } = window.location;
-  if (hostname === 'www.twitch.tv') return handleTwitch();
-  if (hostname === 'www.youtube.com') return handleYoutube();
+	const { hostname } = window.location;
+	if (hostname === "www.youtube.com") return handleYoutube();
 })();
 
 function handleMaximizeShortcut(fn) {
-  return (evt) => {
-    if (!(evt.key === 'Escape' || (evt.altKey && evt.key === 't'))) return;
-    evt.preventDefault();
-    evt.stopPropagation();
-    fn(evt);
-  };
+	return (evt) => {
+		if (!(evt.key === "Escape" || (evt.altKey && evt.key === "t"))) return;
+		evt.preventDefault();
+		evt.stopPropagation();
+		fn(evt);
+	};
 }
 
 function createStyle(css) {
-  const style = document.createElement('style');
-  style.textContent = css;
-  return style;
-}
-
-function handleTwitch() {
-  const style = createStyle(`
-    body.ce-custom-theater .persistent-player {
-      width: 100% !important;
-      z-index: 1000000 !important;
-      height: calc(100vw * 9 / 16) !important;
-    }
-    body.ce-custom-theater .right-column {
-      position: fixed !important;
-      display: block !important;
-      z-index: 9999999 !important;
-      width: 100vw !important;
-      height: auto !important;
-      left: 0 !important;
-      top: calc(100vw * 9 / 16) !important;
-      bottom: 0;
-    }
-    body.ce-custom-theater .right-column > div[class*=Layout-sc]:first-child {
-      display: block !important;
-    }
-    body.ce-custom-theater .right-column .channel-root__right-column {
-      width: 100% !important;
-    }
-  `);
-
-  document.addEventListener(
-    'keydown',
-    handleMaximizeShortcut(() => {
-      const video = document.querySelector('.persistent-player');
-      if (!video) return;
-
-      const theaterBtn =
-        document.querySelector(
-          '[data-a-target="player-theatre-mode-button"]'
-        ) || document.querySelector('.qa-theatre-mode-button');
-      if (!theaterBtn) return;
-
-      if (document.body.classList.contains('ce-custom-theater')) {
-        document.body.classList.remove('ce-custom-theater');
-        style.remove();
-        theaterBtn.click();
-        return;
-      }
-
-      if (video.classList.contains('persistent-player--theatre')) {
-        document.body.classList.add('ce-custom-theater');
-        document.head.appendChild(style);
-        return;
-      }
-
-      theaterBtn.click();
-    }),
-    true
-  );
+	const element = document.createElement("style");
+	element.textContent = css;
+	return element;
 }
 
 function handleYoutube() {
-  const cssInjectedClass = 'css-injected';
-  let maximizeBtn = null;
+	let maximizeBtn = null;
 
-  const style = createStyle(`
+	const style = createStyle(`
     html {
       overflow: hidden;
     }
-    #items, #comments {
+
+    #items,
+    #comments,
+    #related,
+    .ytp-overlays-container,
+    #below,
+    #secondary {
       visibility: hidden !important;
     }
+
     #player,
     #player-theater-container,
-    #player-container-inner {
+    #player-container-inner,
+    #page-manager {
       z-index: 10000000 !important;
       position: fixed !important;
       top: 0 !important;
@@ -105,6 +55,7 @@ function handleYoutube() {
       height: 100% !important;
       width: 100% !important;
     }
+
     .ytp-chrome-bottom {
       left: 12px !important;
       width: auto !important;
@@ -113,9 +64,11 @@ function handleYoutube() {
       bottom: 0 !important;
       margin: 0 !important;
     }
+
     .html5-video-player {
       z-index: 10000;
     }
+
     video.video-stream.html5-main-video {
       background-color: black !important;
       position: fixed !important;
@@ -129,56 +82,54 @@ function handleYoutube() {
     }
   `);
 
-  function toggleFullscreen(evt, { noFullscreen = false } = {}) {
-    if (!maximizeBtn) return;
+	function toggleFullscreen(_evt, { noFullscreen = false } = {}) {
+		if (!maximizeBtn) return;
 
-    const theaterBtn = document.querySelector('.ytp-size-button');
-    const inTheater = !!document.querySelector('ytd-watch-flexy[theater]');
-    const inFullscreen = maximizeBtn.classList.contains(cssInjectedClass);
+		const theaterBtn = document.querySelector(".ytp-size-button");
+		const inFullscreen = document.head.contains(style);
+    const inTheaterMode = !!document.querySelector('ytd-watch-flexy[theater]');
 
-    if (inTheater && !inFullscreen) {
-      if (noFullscreen) {
-        theaterBtn?.click();
-      } else {
-        maximizeBtn.classList.add(cssInjectedClass);
-        document.head.appendChild(style);
-      }
-    } else if (inFullscreen) {
-      maximizeBtn.classList.remove(cssInjectedClass);
-      document.head.removeChild(style);
-    } else {
-      if (noFullscreen) {
-        theaterBtn?.click();
-      } else {
-        maximizeBtn.classList.add(cssInjectedClass);
-        document.head.appendChild(style);
-      }
+		if (noFullscreen) {
+			return void theaterBtn?.click();
+		}
+
+		if (inFullscreen) {
+			document.head.removeChild(style);
+			return void window.dispatchEvent(new Event("resize"));
+		}
+
+    document.head.append(style);
+
+    //
+    // Don't resize on theater mode as the track is already in good size.
+    // We have to trigger a resize in non theater mode (small) so that
+    // the track is updated
+    //
+    if (!inTheaterMode) {
+      window.dispatchEvent(new Event("resize"));
     }
+	}
 
-    window.dispatchEvent(new Event('resize'));
-    evt?.preventDefault();
-  }
+	function attachButton() {
+		const original = document.querySelector(
+			'.ytp-fullscreen-button.ytp-button, button[aria-label*="Full screen" i]',
+		);
+		if (!original || maximizeBtn) return;
 
-  function attachButton() {
-    const original = document.querySelector(
-      '.ytp-fullscreen-button.ytp-button, button[aria-label*="Full screen" i]'
-    );
-    if (!original || maximizeBtn) return;
+		maximizeBtn = original.cloneNode(true);
+		original.parentNode?.insertBefore(maximizeBtn, original.nextSibling);
+		maximizeBtn.addEventListener("click", toggleFullscreen);
+	}
 
-    maximizeBtn = original.cloneNode(true);
-    original.parentNode?.insertBefore(maximizeBtn, original.nextSibling);
-    maximizeBtn.addEventListener('click', toggleFullscreen);
-  }
+	const observer = new MutationObserver(attachButton);
+	observer.observe(document.body, { childList: true, subtree: true });
+	attachButton();
 
-  const observer = new MutationObserver(attachButton);
-  observer.observe(document.body, { childList: true, subtree: true });
-  attachButton();
-
-  document.addEventListener(
-    'keydown',
-    handleMaximizeShortcut((evt) => {
-      if (!maximizeBtn) attachButton();
-      toggleFullscreen(undefined, { noFullscreen: evt.shiftKey });
-    })
-  );
+	document.addEventListener(
+		"keydown",
+		handleMaximizeShortcut((evt) => {
+			if (!maximizeBtn) attachButton();
+			toggleFullscreen(undefined, { noFullscreen: evt.shiftKey });
+		}),
+	);
 }
